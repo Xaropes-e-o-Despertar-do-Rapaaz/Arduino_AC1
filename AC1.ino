c9699241531be6dc8d4720f2df7bc75a4eaa3804
Gabriela Ramalho <81374304+gabrielaramalho@users.noreply.github.com>
2021-03-29 21:59:19 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,11 +1,12 @@
 //variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
+const int azul = 7;
 
 bool estadoLedVermelho = false;
 
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
 const int botaoDelay = 100;
 
@@ -16,27 +17,32 @@ void setup()
   
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
+  pinMode(azul, OUTPUT);
   
   Serial.begin(9600);
 	
   Serial.println("AC1 - projeto fabrica farmaceutica");
   Serial.println("                           V1.0");
-  Serial.println("Grupo: xaropinho e o despertar do raizz             ");
+  Serial.println("Grupo: xaropinho e o despertar do rapazz             ");
 }
 
 void loop()
 {
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
   	Serial.println("botao 1 apertado");
-    ledVermelho();
+    ledVermelho(true);
+  	lastDebounceTime1 = millis();
+	  
+	  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao2)){
+  	Serial.println("botao 2 apertado");
+    ledVermelho(false);
   	lastDebounceTime1 = millis();
   }
   
   if(getTemperatura() > 15){
     ledAzul(true);
   }else{
-  	ledAzul(false); 
+  ledAzul(false); 
   }
   	
   delay(10);
